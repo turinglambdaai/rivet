5e4d65de1bdaea652890e44d85539edf993de809
TuringLambdaAI <jirentianxiang1024@gmail.com>
2026-09-18 00:21:34 +0800

    feat(winui): add native WinUI host shell

---
+++ platform/windows/host/pch.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#ifndef NOMINMAX
+#define NOMINMAX
+#endif
+#include <windows.h>
+#include <unknwn.h>
+#include <restrictederrorinfo.h>
+#include <hstring.h>
+
+#undef GetCurrentTime
+
+#include <winrt/Windows.Foundation.h>
+#include <winrt/Windows.ApplicationModel.Activation.h>
+#include <winrt/Microsoft.UI.Dispatching.h>
+#include <winrt/Microsoft.UI.Xaml.h>
+#include <winrt/Microsoft.UI.Xaml.Controls.h>
+#include <winrt/Microsoft.UI.Xaml.Controls.Primitives.h>
+#include <winrt/Microsoft.UI.Xaml.Markup.h>
+
+#include <atomic>
+#include <filesystem>
+#include <future>
+#include <memory>
+#include <string>
+#include <utility>
+
+#include "../runtime/backend.hpp"
