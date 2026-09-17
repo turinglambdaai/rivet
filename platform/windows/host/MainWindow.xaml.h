800602dc6d6ad00304f110fc371ddbac42028e21
TuringLambdaAI <jirentianxiang1024@gmail.com>
2026-09-18 00:22:32 +0800

    feat(winui): connect window to Rivet backend

---
+++ platform/windows/host/MainWindow.xaml.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include "pch.h"
+#include "MainWindow.g.h"
+
+namespace winrt::RivetHost::implementation {
+
+struct MainWindow : MainWindowT<MainWindow> {
+  MainWindow();
+
+  void Increment_Click(winrt::IInspectable const& sender,
+                       Microsoft::UI::Xaml::RoutedEventArgs const& args);
+
+ private:
+  winrt::fire_and_forget InitializeBackendAsync();
+  winrt::fire_and_forget IncrementAsync();
+  void SetReadyUi();
+  void SetErrorUi(std::string const& message);
+
+  std::unique_ptr<rivet::windows::Backend> backend_;
+  std::atomic<std::int64_t> count_{0};
+};
+
+}  // namespace winrt::RivetHost::implementation
+
+namespace winrt::RivetHost::factory_implementation {
+
+struct MainWindow : MainWindowT<MainWindow, implementation::MainWindow> {};
+
+}  // namespace winrt::RivetHost::factory_implementation
