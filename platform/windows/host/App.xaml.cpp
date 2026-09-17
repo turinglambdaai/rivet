6bf1cb909cb44a2b08f028de35b02a5225a59530
TuringLambdaAI <jirentianxiang1024@gmail.com>
2026-09-18 00:22:02 +0800

    feat(winui): launch Rivet window

---
+++ platform/windows/host/App.xaml.cpp
@@ -0,0 +1,27 @@
+#include "pch.h"
+#include "App.xaml.h"
+#include "MainWindow.xaml.h"
+
+namespace winrt::RivetHost::implementation {
+
+App::App() {
+  InitializeComponent();
+
+#if defined(_DEBUG) && !defined(DISABLE_XAML_GENERATED_BREAK_ON_UNHANDLED_EXCEPTION)
+  UnhandledException([](winrt::IInspectable const&,
+                        Microsoft::UI::Xaml::UnhandledExceptionEventArgs const& e) {
+    if (::IsDebuggerPresent()) {
+      auto const message = e.Message();
+      (void)message;
+      __debugbreak();
+    }
+  });
+#endif
+}
+
+void App::OnLaunched(Microsoft::UI::Xaml::LaunchActivatedEventArgs const&) {
+  window_ = winrt::make<MainWindow>();
+  window_.Activate();
+}
+
+}  // namespace winrt::RivetHost::implementation
