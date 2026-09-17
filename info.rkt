35cae00a08df2a7c67e9f4497fb529955a81dd4f
TuringLambdaAI <jirentianxiang1024@gmail.com>
2026-09-18 00:06:31 +0800

    feat: add Rivet package entry point

---
+++ info.rkt
@@ -0,0 +1,18 @@
+#lang info
+
+(define collection 'multi)
+(define version "0.0.1")
+(define pkg-desc "Native desktop application foundation for Racket")
+(define pkg-authors '(turinglambdaai))
+(define license '(MIT))
+
+(define deps
+  '("base"
+    "rackunit-lib"))
+
+(define build-deps
+  '("scribble-lib"
+    "racket-doc"))
+
+(define raco-commands
+  '(("rivet" rivet-cli/main "create, build, and run Rivet applications" #f)))
