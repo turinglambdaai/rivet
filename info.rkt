eaf3321adff653e8693543627bfaa5b61e6ca992
TuringLambdaAI <jirentianxiang1024@gmail.com>
2026-09-18 09:49:36 +0800

    chore: prepare Rivet 0.1.0 package metadata

--- info.rkt
+++ info.rkt
@@ -1,19 +1,17 @@
 #lang info
 
 (define collection 'multi)
-(define version "0.0.1")
+(define version "0.1.0")
 (define pkg-desc "Native desktop application foundation for Racket")
 (define pkg-authors '(turinglambdaai))
 (define license '(MIT))
 
 (define deps
   '("base"
-    "cext-lib"
-    "rackunit-lib"))
+    "cext-lib"))
 
 (define build-deps
-  '("scribble-lib"
-    "racket-doc"))
+  '("rackunit-lib"))
 
 (define raco-commands
   '(("rivet" rivet-cli/main "create, build, and run Rivet applications" #f)))
