96c49e4f49f283065b50811f0aa76fccb53e689b
TuringLambdaAI <jirentianxiang1024@gmail.com>
2026-09-20 21:34:51 +0800

    Test: generate Swift and C++ Record DTOs

--- tests/codegen.rkt
+++ tests/codegen.rkt
@@ -26,6 +26,11 @@
 
 (provide start)
 
+(define-record User
+  ([id : Int64]
+   [display-name : String]
+   [nickname : (Optional String)]))
+
 (define-state counter : Int64 0)
 
 (define-rpc (greet [name String] : String)
@@ -34,6 +39,9 @@
 (define-rpc (increment [value Int64] : Int64)
   (add1 value))
 
+(define-rpc (echo-user [user : User] : User)
+  user)
+
 (define (start in-fd out-fd)
   (serve-fds in-fd out-fd))
 RKT
@@ -41,8 +49,9 @@ RKT
 
     (define project (load-project project-root))
     (define schema (generate-clients! project))
-    (check-equal? (length (first schema)) 2)
+    (check-equal? (length (first schema)) 3)
     (check-equal? (length (second schema)) 1)
+    (check-equal? (length (third schema)) 1)
 
     (define swift
       (file->string
@@ -52,10 +61,19 @@ RKT
       (file->string
        (build-path project-root "windows" "GeneratedBackend.hpp")))
 
+    (check-true (regexp-match? #rx"public struct User: Sendable" swift))
+    (check-true (regexp-match? #rx"public let display_name: String" swift))
+    (check-true (regexp-match? #rx"public let nickname: String\\?" swift))
+    (check-true (regexp-match? #rx"func echo_user\\(user: User\\) async throws -> User" swift))
     (check-true (regexp-match? #rx"func greet\\(name: String\\)" swift))
     (check-true (regexp-match? #rx"func increment\\(value: Int64\\)" swift))
     (check-true (regexp-match? #rx"func getCounter\\(\\) async throws -> Int64" swift))
     (check-true (regexp-match? #rx"func setCounter\\(_ value: Int64\\)" swift))
+
+    (check-true (regexp-match? #rx"struct User" cpp))
+    (check-true (regexp-match? #rx"std::string display_name;" cpp))
+    (check-true (regexp-match? #rx"std::optional<std::string> nickname;" cpp))
+    (check-true (regexp-match? #rx"std::future<User> echo_user\\(User user\\)" cpp))
     (check-true (regexp-match? #rx"std::future<std::string> greet" cpp))
     (check-true (regexp-match? #rx"std::future<std::int64_t> increment" cpp))
     (check-true (regexp-match? #rx"std::future<std::int64_t> get_counter\\(\\)" cpp))
